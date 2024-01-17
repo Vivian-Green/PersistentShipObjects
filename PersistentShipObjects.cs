41b14a22f74b47bec58a61771c361a6edb651c3a
Gavin Jett <42091882+ghxc2@users.noreply.github.com>
2024-01-17 16:28:15 -0500

    gh rewrite (#1)
    * Rewrite to shorten and work on errors
    
    * Delete PersistentShipObjects.csproj
    
    * Delete JsonIO.cs

--- PersistentShipObjects.cs
+++ PersistentShipObjects.cs
@@ -11,7 +11,11 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.ComponentModel;
 using System.Collections;
-
+using LethalCompanyTemplate;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
 
 // todo: ask chatgpt for this lmao
 
@@ -29,7 +33,6 @@ using System.Collections;
 
 
 namespace PersistentShipObjects {
-    [BepInDependency("evaisa.lethallib", "0.12.1")]
     [BepInPlugin(GUID, NAME, VERSION)]
 
     public class PersistentShipObjects : BaseUnityPlugin {
@@ -38,261 +41,105 @@ namespace PersistentShipObjects {
         private const string VERSION = "0.1.2";
 
         public static ManualLogSource MLS { get; private set; }
-
         private readonly Harmony harmony = new Harmony(GUID);
         public static PersistentShipObjects instance;
+       
+        
+        // Location for Saved Objects
+        public static string ObjTransformsPath = Application.persistentDataPath + "/PersistentShipObjects/Objects.json";
+        public static string ObjTransformsFolderPath = Application.persistentDataPath + "/PersistentShipObjects";
 
-        public static ConfigEntry<bool> doDebugPrints;
-        public static ConfigEntry<int> maxSaveFrequency;
-
-        public static Dictionary<string, Transform> ObjTransforms;
-        public static Dictionary<string, Transform> LastSavedObjTransforms;
-        public static string ObjTransformsPath = Application.persistentDataPath + "/PersistentShipObjects/ObjTransforms.json";
-
-        GameObject transformHolder = new GameObject("transformHolder");
+        // Object Manager
+        public static List<TransformObjects> TransformObjectsManager;
 
         public void Awake() {
-            JsonIO.Awake();
 
-            MLS = Logger;
-            MLS.LogInfo("PersistentShipObjects instantiated!");
+            Debug.Log(ObjTransformsPath);
 
-            if (instance == null) {
-                instance = this;
+            if (!File.Exists(ObjTransformsFolderPath))
+            {
+                Directory.CreateDirectory(ObjTransformsFolderPath);
             }
 
-            MLS.LogInfo("about to load from json");
-            MLS.LogInfo(ObjTransformsPath);
-            ObjTransforms = JsonIO.LoadFromJson(ObjTransformsPath);
-
-            if (ObjTransforms == null) {
-                MLS.LogError("JsonIO.LoadFromJson(ObjTransformsPath) returned null!");
-            } else {
-                MLS.LogInfo("loaded from json");
+            if (!File.Exists(ObjTransformsPath))
+            {
+                File.Create(ObjTransformsPath).Dispose();
             }
 
-            doDebugPrints = Config.Bind(
-                "Debug",
-                "doDebugPrints",
-                false,
-                "should PersistentShipObjects paint the console yellow?"
-            );
-            maxSaveFrequency = Config.Bind(
-                "General",
-                "maxSaveFrequency",
-                15,
-                "how many seconds have to pass between saving objects? (it will just queue them in between saves to not cause lag with a lot of shipObject movements)"
-            );
+            TransformObjectsManager = new List<TransformObjects>();
+            ReadJSON();
 
             //Config.Save();
-
+            
             Harmony.CreateAndPatchAll(typeof(ShipBuildModeManagerPatch));
         }
 
-
-        public static bool SaveObjTransform(string name, Transform trans) {
-            DebugLog("saving trans of obj named: " + name);
-
-            DebugLog("about to check if is host-");
-            if (!RoundManager.Instance.NetworkManager.IsHost) {
-                MLS.LogInfo("something was moved, but this isn't my ship! So I'll just pretend I didn't see that.");
-                return false;
+        // Write to file, should be called every time an update occurs
+        // Updates occur on placement editing
+        public static void SaveObjTransform() {
+            var str = JsonConvert.SerializeObject(TransformObjectsManager);
+            Debug.Log("JSON: ");
+            Debug.Log(str);
+            File.WriteAllText(ObjTransformsPath, str);
+        }
+        
+        // Finds object in manager via UnlockableID searching ObjectManager
+        public static TransformObjects GetObjects(int unlockableID)
+        {
+            foreach(TransformObjects obj in TransformObjectsManager)
+            {
+                if (obj.unlockableID == unlockableID) 
+                {
+                    return obj;
+                }
             }
-            if (trans == null) {
-                MLS.LogWarning("SaveObjTransform: received transform is null!");
-                return false;
-            }
-            DebugLog("I am host and transform isn't null!");
-            DebugLog(trans.position);
-            DebugLog(trans.rotation);
-
-            DebugLog("before: ", 'e');
-            foreach (KeyValuePair<string, Transform> kvp in ObjTransforms) {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
-
-            // ContainsKey ? set : add
-            if (ObjTransforms.ContainsKey(name)) {
-                DebugLog("key found in ObjTransforms!");
-                ObjTransforms[name] = PosAndRotAsGOWithTransform(trans.position, trans.rotation).transform;
-            } else {
-                DebugLog("key NOT found in ObjTransforms!");
-                ObjTransforms.Add(name, PosAndRotAsGOWithTransform(trans.position, trans.rotation).transform);
-            }
-            DebugLog("after: ", 'e');
-            foreach (KeyValuePair<string, Transform> kvp in ObjTransforms) {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
-
-            Task.Run(async () => {
-                await Task.Delay((int)(1000));
-                await JsonIO.QueueSaveToJson(ObjTransformsPath, ObjTransforms, maxSaveFrequency.Value); // not awaited-
-            });
-
-            DebugLog("saved object transform (to memory-)");
-            return true; // it *did* save to ObjTransforms- but whether or not it *saves* saves-
+            return null;
         }
 
-
-        public static GameObject PosAndRotAsGOWithTransform(Vector3 position, Quaternion rotation) {
-            Debug.Log("entering PosAndRotAsGOWithTransform");
-            GameObject go = new GameObject("transformHolder");
-            Transform trans = go.transform;
-
-            go.transform.position = position;
-            go.transform.rotation = rotation;
-            go.transform.localScale = Vector3.one;
-            return go;
-        }
-
-
-        public static void DebugLog(System.Object logMessage, char logType = 'i') {
-            if (doDebugPrints.Value != true) return; // todo: is this truthy or boolean- how does C# work again lmao. does !doDebugPrints.Value give left or right:
-                                                     // true: true                  true: true
-                                                     // false: false                false: true
-                                                     // null: false                 null: false
-            String LogString = logMessage.ToString();
-
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            switch (logType) {
-                case 'w':
-                    //Console.ForegroundColor = ConsoleColor.;
-                    MLS.LogWarning("PersistentShipObjects: " + LogString);
-                    break;
-                case 'm':
-                    MLS.LogMessage("PersistentShipObjects: " + LogString);
-                    break;
-                case 'e':
-                    MLS.LogError("PersistentShipObjects: " + LogString);
-                    break;
-                case 'i':
-                    MLS.LogInfo("PersistentShipObjects: " + LogString);
-                    break;
-                default:
-                    MLS.LogWarning("DebugLogger received an invalid log type, but here's whatever this is:");
-                    DebugLog("    " + LogString, 'w');
-                    break;
-            }
-            //Console.WriteLine("PersistentShipObjects: " + LogString);
-            Console.ResetColor();
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        [HarmonyPatch(typeof(StartOfRound), "SpawnUnlockable")]
-        [HarmonyPostfix]
-        static void Postfix_SpawnUnlockable(int unlockableIndex, GameObject gameObject, UnlockablesList unlockablesList) {
-            UnlockableItem unlockableItem = unlockablesList.unlockables[unlockableIndex];
-
-            Debug.LogWarning("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
-            Debug.Log(gameObject.GetType() + " named " + gameObject.name);
-            Debug.Log(gameObject.transform.parent?.gameObject.GetType() + " named " + gameObject.transform.parent?.gameObject.name);
-
-            switch (unlockableItem.unlockableType) {
-                case 0:
-                    // Handle case 0 if needed
-                    break;
-                case 1:
-                    // Handle case 1
-                    if (PersistentShipObjects.ObjTransforms.ContainsKey(gameObject.name)) { // if name in config, overwrite trans
-                        Transform savedTrans = PersistentShipObjects.ObjTransforms[gameObject.name];
-                        gameObject.transform.position = savedTrans.position; // probably needs to be parent
-                        gameObject.transform.rotation = savedTrans.rotation;
-                    }
-                    break;
-                    // Add more cases if necessary
-            }
-        }
-
-
-
-
-
-
-        /*
-        //LoadUnlockables
-        [HarmonyPatch(typeof(StartOfRound), "LoadShipGrabbableItems")]
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
-            int tfIndexInLocalVariablesIsHidingMyGotDangComponent = 0;
-            var codes = new List<CodeInstruction>(instructions);
-
-            //
-            /* ok bitch fucker, look at this shit in "Lethal Company\Lethal Company_Data\Managed\Assembly-CSharp.dll" StartOfRound.LoadShipGrabbableItems() (viewed with ILSpy, IL with C#):
-
-            // GrabbableObject component = UnityEngine.Object.Instantiate(allItemsList.itemsList[array[i]].spawnPrefab, array2[i], Quaternion.identity, elevatorTransform).GetComponent<GrabbableObject>();
-            [many skipped IL instructions]
-            IL_01ba: callvirt instance !!0 [UnityEngine.CoreModule]UnityEngine.GameObject::GetComponent<class GrabbableObject>()
-            IL_01bf: stloc.0
-            // component.fallTime = 1f;
-
-            This makes as component, then stloc (pop from stack (which is the return of the expression, an GrabbableObject- well a pointer to one))'s it into the 0th local variable. 
-            This is 17 lines before our injection, at the time of writing this comment (Jan 10, 2024)
-
-            You are welcome, whoever is reading this. Should this break (and it likely will, as it's trans(based; and red)piled), maybe someone (future viv-) will know 
-            where tf to look to find that index in the future.
-            //*//*
-
-            DebugLog("this motherfucker is straight TRANSpiling which is poggies");
-            for (int i = 0; i < codes.Count; i++) {
-                //DebugLog(codes[i].ExtractLabels());
-
-                if (!(codes[i].opcode == OpCodes.Callvirt && codes[i].operand.ToString().Contains("Spawn"))) continue;
-                // just before component.NetworkObject.Spawn();
-
-                DebugLog("yo bitch fucker, i is " + i + " when shit gets insertamalated, we injecting UpdateGrabbableObjTrans() over here at " + i + " fr fr");
-                codes.Insert(i,
-                    new CodeInstruction(
-                        OpCodes.Ldloc_S, // load to stack from local variable at index of
-                        tfIndexInLocalVariablesIsHidingMyGotDangComponent // this
-                    )
-                );
-                codes.Insert(i + 1,
-                    new CodeInstruction(
-                        OpCodes.Call,
-                        AccessTools.Method(
-                            typeof(PersistentShipObjects),
-                            "UpdateGrabbableObjTrans"
-                        )
-                    )
-                );
+        // Updates ObjectsManager with each update
+        public static void UpdateObjectManager(TransformObjects newObj)
+        {
+            if (newObj == null)
+            {
+                Debug.Log("NULL OBJECT??");
+                return;
             }
 
-            return codes.AsEnumerable();
+            foreach(TransformObjects obj in TransformObjectsManager)
+            {
+                if (!TransformObjectsManager.Any())
+                {
+                    TransformObjectsManager.Add(newObj);
+                    break;
+                }
+
+                if (obj.unlockableID == newObj.unlockableID)
+                {
+                    TransformObjectsManager.Remove(obj);
+                    break;
+                }
+            }
+
+            TransformObjectsManager.Add(newObj);
+            SaveObjTransform();
         }
 
-        public static void UpdateGrabbableObjTrans(GrabbableObject component) {
-            DebugLog("ayo this fucker just got injected lmao, also " + component.name + " says go fuck yourself");
-
-            // todo: check if this name of obj has like, been moved already?
-            if (PersistentShipObjects.ObjTransforms.ContainsKey(component.name)) { // if name in config, overwrite trans
-                Transform savedTrans = PersistentShipObjects.ObjTransforms[component.name];
-                component.transform.position = savedTrans.position; // probably needs to be parent of
-                component.transform.rotation = savedTrans.rotation;
+        // Inits ObjectManager from file
+        public static void ReadJSON()
+        {
+            try
+            {
+                string json = File.ReadAllText(ObjTransformsPath);
+                List<TransformObjects> objs = JsonConvert.DeserializeObject<List<TransformObjects>>(json);
+                foreach (TransformObjects obj in objs)
+                {
+                    TransformObjectsManager.Add(obj);
+                }
+            } catch (Exception e)
+            {
+                //
             }
-        }*/
+            
+        }
     }
 }
